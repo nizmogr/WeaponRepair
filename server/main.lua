069d1351f5924760b2dc93a22b0df3a498e26a4c
nizmogr <64306122+nizmogr@users.noreply.github.com>
2022-05-09 19:57:27 +0300

    changed weapon check
    Removed lib.callback to net event only cause I didn't know how to use lib.callback and couldn't send args to it
    Modified the way that it receives args of weapon chosen by players to repair

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,10 @@
 local ox_inventory = exports.ox_inventory
 
-lib.callback.register('WRepair:Cost', function(weapon)
-    ox_inventory:RemoveItem(source, 'repairkit_weapon', 1) -- "repairkit_weapon" is the item you need to repair. You may change it to fit your needs,
-    local weapon = ox_inventory:GetCurrentWeapon(source)
-    if weapon then
-        ox_inventory:SetDurability(source, weapon.slot, 100)
-    end
+RegisterNetEvent('WRepair:Cost')
+AddEventHandler('WRepair:Cost', function(data)
+    local curweapon = data.selweapon
+    local weapon = ox_inventory:Search(source, 'slots', curweapon)
+    weapon = weapon[1]
+    ox_inventory:RemoveItem(source, 'repairkit_weapon', 1) --You can change 'repairkit_weapon' to whatever item you want to remove as payment
+    ox_inventory:SetDurability(source, weapon.slot, 100)
 end)
